79155c50fa4250d4d756ba09b0dd3b585f86a86f
EchoPrograms <brogan.oberhaus30@gmail.com>
2025-02-19 19:07:27 -0500

    IP change - BO

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { NT4Provider } from '@frc-web-components/react';
 // TODO: change the address to the address of the robot when not testing 10.31.40.2
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
-    <NT4Provider address="localhost"> 
+    <NT4Provider address="10.31.40.2"> 
       <App />
     </NT4Provider>
   </React.StrictMode>,
